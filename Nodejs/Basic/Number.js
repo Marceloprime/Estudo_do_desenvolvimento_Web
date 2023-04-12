d9300b1ac952d091b8480dc61d1bf68f2ff0cedd
Marcelo_A_Reis <marcelo.reis@mblabs.com.br>
2023-04-12 08:40:37 -0300

    add notes and exercives

---
+++ Nodejs/Basic/Number.js
@@ -0,0 +1,23 @@
+// type number
+
+const number = Number(4)
+console.log(number.toPrecision(2))
+// operation
+let x1 = 2
+let x2 = 3
+
+console.log(x1-x2)
+console.log(x1+x2)
+console.log(x1*x2)
+console.log(x1/x2)
+
+// ponto flutuante
+x1 = 2.4
+x2 = 3.5
+
+console.log(x1-x2)
+console.log(x1+x2)
+console.log(x1*x2)
+console.log(x1/x2)
+
+// NaN -> Not a Number
---
+++ Nodejs/Basic/Strings.js
@@ -0,0 +1,9 @@
+const text = '1'
+
+const word = String('Teste')
+
+console.log(word)
+console.log(`at: ${word.at(2)}`)
+console.log(`charAt: ${word.charAt(2)}`)
+console.log(`charCodeAt: ${word.charCodeAt(3)}`)
+console.log(`codePointAt: ${word.codePointAt(3)}`)
